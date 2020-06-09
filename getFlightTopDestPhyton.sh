57b2cc5b6f9b02300e308152f0bbeae51d2dae06
robertoheinzenca <roberto.heinzen@rcservti.com.br>
2020-06-08 20:44:11 -0700

    Second Commit

---
+++ getFlightTopDestPhyton.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 
+#"PHX" 
+
+python3 -<<'END_SCRIPT' 
+import pandas as pd
+flights = pd.read_csv("2007_aa")
+topsDest = pd.DataFrame({'Dest':flights['Dest'],'#Arrivals':flights['UniqueCarrier']}).groupby('Dest').count() 
+print(topsDest.sort_values('#Arrivals',ascending = False).head(3))
+END_SCRIPT
